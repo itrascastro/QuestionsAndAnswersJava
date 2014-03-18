ee1e3a44094b754473ebb5d390511b845a8a6d78
itrascastro <i.trascastro@gmail.com>
2014-03-18 19:38:45 +0100

    Juego Dados

---
+++ src/uf2/oop/dados/Dado.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uf2.oop.dados;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ismael trascastro
+ */
+public class Dado 
+{
+    private int numero;
+
+    public Dado() 
+    {
+    }
+
+    public int getNumero() 
+    {
+        return numero;
+    }
+
+    public void setNumero(int numero) 
+    {
+        this.numero = numero;
+    }
+    
+    public void lanzar()
+    {
+        Random aleatorio = new Random();
+        
+        this.numero = aleatorio.nextInt(6) + 1;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Dado[" + this.numero + "]";
+    }
+}
---
+++ src/uf2/oop/dados/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uf2.oop.dados;
+
+/**
+ *
+ * @author ismael trascastro
+ */
+public class Test 
+{
+    public static void main(String[] args)
+    {
+        Dado d = new Dado();
+        
+        d.lanzar();
+        
+        System.out.println(d);
+    }
+}

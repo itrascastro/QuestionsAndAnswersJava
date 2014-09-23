1248b61b8abf984d2ac81deab850675b84a50a01
itrascastro <i.trascastro@gmail.com>
2014-09-23 20:54:15 +0200

    uf1

---
+++ src/uf1/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uf1;
+
+/**
+ *
+ * @author ismael trascastro
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+        
+        int resultado;
+        
+        resultado = n1 + n2;
+        
+        System.out.println("El resultado es: " + resultado);
+    }
+}
